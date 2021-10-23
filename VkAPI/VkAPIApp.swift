0b3eac94e966a53406f40fb5baa2f9ca82af146e
Marek Roslik <marek.roslik@gmail.com>
2021-10-23 16:26:55 +0300

    Initial Commit

---
+++ VkAPI/VkAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VkAPIApp.swift
+//  VkAPI
+//
+//  Created by Marek Roslik on 23.10.21.
+//
+
+import SwiftUI
+
+@main
+struct VkAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
